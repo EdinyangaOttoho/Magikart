af7963611f7ba6a9f9df401c7cd0ce20a7482b17
Edinyanga O. Ottoho <elzucky@gmail.com>
2021-08-10 14:34:58 +0100

    Dotenv

--- connect.php
+++ connect.php
@@ -1,3 +1,4 @@
 <?php
-    $db = mysqli_connect("localhost", "root", "", "magikart");
+    require __DIR__ . '/dotenv-loader.php';
+    $db = mysqli_connect($_ENV['MYSQLI_HOST'], $_ENV['MYSQLI_USER'], $_ENV['MYSQLI_PASSWORD'], $_ENV['MYSQLI_DATABASE']);
 ?>
