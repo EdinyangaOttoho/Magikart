d47dd56c58dbc7d9c532b5acd2faa25170516531
Edinyanga O. Ottoho <elzucky@gmail.com>
2021-08-12 12:12:29 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,5 @@
 <?php
+    require __DIR__ . '/vendor/autoload.php';
     require __DIR__ . '/dotenv-loader.php';
     $db = mysqli_connect($_ENV['MYSQLI_HOST'], $_ENV['MYSQLI_USER'], $_ENV['MYSQLI_PASSWORD'], $_ENV['MYSQLI_DATABASE']);
 ?>
